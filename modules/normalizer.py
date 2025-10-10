986e21ce6ed628d426b721ed52929d784d572240
Caleb Mok <2689719m@student.gla.ac.uk>
2025-10-10 18:44:47 +0800

    added and linked normalizer module to parser module

---
+++ modules/normalizer.py
@@ -0,0 +1,5 @@
+class Normalizer:
+    def normalize(self, param_autos):
+        # take param_autos dict and make sure the keys are normalised before doing comparison with param_inputs
+        print("Normalizing test names")
+        return param_autos
--- modules/parser.py
+++ modules/parser.py
@@ -1,5 +1,14 @@
+from modules.normalizer import Normalizer
+
 class PDFParser:
     def parse(self, filepath):
         # return dict of extracted {param_name: value}
-        print("parsing")
-        return {}
+        self.normalizer = Normalizer()
+
+        # get pre-normalized dict from filepath
+        pre_dict = {}
+
+        param_autos = self.normalizer.normalize(pre_dict)
+       
+        print("Parsing")
+        return param_autos
